17c07f2f551868e549f53a1d87f924c1962b9b59
L950x <loick.lp59@gmail.com>
2023-12-19 18:43:39 +0100

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -26,42 +26,42 @@ function App() {
 
 
   // Disabled for github deployement -->
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (token && token.split('.').length === 3) {
-      const decodedToken = jwtDecode(token);
-      const expirationDate = decodedToken.exp;
-      const currentTimestamp = Math.floor(Date.now() / 1000);
+  // useEffect(() => {
+  //   const token = localStorage.getItem("token");
+  //   if (token && token.split('.').length === 3) {
+  //     const decodedToken = jwtDecode(token);
+  //     const expirationDate = decodedToken.exp;
+  //     const currentTimestamp = Math.floor(Date.now() / 1000);
 
-      if (expirationDate > currentTimestamp) {
-        const userId = decodedToken.id;
-        Axios.post(`/isLogged/${userId}`, {}, {
-          headers: {
-            Authorization: token
-          }
-        }).then((response) => {
-          if (response.data.status === 0) {
-            localStorage.removeItem("token");
-            if (window.location.pathname !== "/QPanel/login") {
-              window.location.href = "/QPanel/login";
-              toast.error("Please login.");
-            }
-          }
-        });
-      } else {
-        localStorage.removeItem("token");
-        if (window.location.pathname !== "/QPanel/login") {
-          toast.error("Your session has expired. Please login again.");
-          window.location.href = "/QPanel/login";
-        }
-      }
-    } else {
-      if (window.location.pathname !== "/QPanel/login") {
-        window.location.href = "/QPanel/login";
-        toast.error("You are not logged in. Please login.");
-      }
-    }
-  }, []);
+  //     if (expirationDate > currentTimestamp) {
+  //       const userId = decodedToken.id;
+  //       Axios.post(`/isLogged/${userId}`, {}, {
+  //         headers: {
+  //           Authorization: token
+  //         }
+  //       }).then((response) => {
+  //         if (response.data.status === 0) {
+  //           localStorage.removeItem("token");
+  //           if (window.location.pathname !== "/QPanel/login") {
+  //             window.location.href = "/QPanel/login";
+  //             toast.error("Please login.");
+  //           }
+  //         }
+  //       });
+  //     } else {
+  //       localStorage.removeItem("token");
+  //       if (window.location.pathname !== "/QPanel/login") {
+  //         toast.error("Your session has expired. Please login again.");
+  //         window.location.href = "/QPanel/login";
+  //       }
+  //     }
+  //   } else {
+  //     if (window.location.pathname !== "/QPanel/login") {
+  //       window.location.href = "/QPanel/login";
+  //       toast.error("You are not logged in. Please login.");
+  //     }
+  //   }
+  // }, []);
   // <-- 
 
   return (
