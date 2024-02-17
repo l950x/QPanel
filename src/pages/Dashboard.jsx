c31e0ae2f9398ffdce2e438fdc76458cbf680ced
L950x <loick.lp59@gmail.com>
2024-02-17 02:57:02 +0100

    fix

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -7,7 +7,7 @@ import { ThreeCircles } from "react-loader-spinner";
 import { toast } from "react-toastify";
 import Axios from "./callAxios";
 import axios from "axios";
-import Sidebar from "../components/sidebar";
+// import Sidebar from "../components/sidebar";
 const Dashboard = () => {
   const sms = async (e) => {
     e.preventDefault();
